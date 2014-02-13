28d59ec1c7c1ae75648cdae57c19ce0f0400019c
Tekin <tekin@tekin-VirtualBox.(none)>
2014-02-13 13:33:00 +0200

    Updated

--- public/js/admin.js
+++ public/js/admin.js
@@ -35,3 +35,18 @@
       }
       }
       }
+      $(window).scroll(function() {
+		if($(window).scrollTop() + $(window).height() == $(document).height()) 
+		{
+		       alert("bottom!");
+		}
+      });
+      
+
+
+
+
+
+
+
+
