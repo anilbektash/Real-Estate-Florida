c6b560cd8a360604f9dfb1c0fa9239e8f88004c9
bektanil <bektas.anil@gmail.com>
2014-02-13 14:30:39 +0200

    Updated

--- query/select.js
+++ query/select.js
@@ -15,8 +15,9 @@ connection.connect(function(err){
 //selects 10 estates starting from index*10 for listing purpose
 exports.select10Estate = function(index, callback){
     connection.query(
-        "select name, area, street_name, city, price from estate, location, text order by date desc, cur_time desc limit ?, ?", [index*10, (index + 1)*10], function(err, res, fields){
+        "select name, area, street_name, city, price from estate inner join location on location.id = estate.id order by date desc, cur_time desc limit ?, ?", [index*10, (index + 1)*10], function(err, res, fields){
             if(res !== undefined){
+                console.log("In get listing... with index " + index);
                 console.log(JSON.stringify(res));
                 callback(res);
             }
